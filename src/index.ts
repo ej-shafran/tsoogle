35f1d06c301eb3ff349283652e6295544ce45491
ej-shafran <ejshafran@hilma.tech>
2023-08-21 12:19:32 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { command, run } from "cmd-ts";
+
+const app = command({
+  name: "tsoogle",
+  args: {},
+  async handler() {
+    console.log("Hello, world!");
+  },
+});
+
+run(app, process.argv.slice(2));
