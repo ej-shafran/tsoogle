6d5c12fda829e5f0fc1002bdfa88b79d9c137eb9
ej-shafran <ejshafran@hilma.tech>
2023-08-21 12:48:38 +0300

    Add Levenshtein distance

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
-import { command, run } from "cmd-ts";
+// import ts from "typescript";
+import { command, positional, run, string } from "cmd-ts";
+import { levDistance } from "./lev-distance";
+// import { ExistingPath } from "cmd-ts/batteries/fs";
 
-const app = command({
-  name: "tsoogle",
-  args: {},
-  async handler() {
-    console.log("Hello, world!");
+const levApp = command({
+  name: "lev-distance",
+  args: {
+    a: positional({ type: string }),
+    b: positional({ type: string }),
+  },
+  handler({ a, b }) {
+    const result = levDistance(a, b);
+    console.log(
+      `the Levenshtein Distance between \`${a}\` and \`${b}\` is ${result}`
+    );
   },
 });
 
-run(app, process.argv.slice(2));
+// const app = command({
+//   name: "tsoogle",
+//   args: {
+//     fileName: positional({ type: ExistingPath }),
+//   },
+//   async handler({ fileName }) {
+//     const program = ts.createProgram({ rootNames: [fileName], options: {} });
+//
+//     console.log("fileName: ", fileName);
+//   },
+// });
+//
+run(levApp, process.argv.slice(2));
---
+++ src/lev-distance.ts
@@ -0,0 +1,53 @@
+import { randomBytes } from "crypto";
+
+function cached<TParams extends unknown[], TReturn>(
+  func: (...params: TParams) => TReturn
+): (...params: TParams) => TReturn {
+  const cache = new Map<string, TReturn>();
+  const sep = randomBytes(12).toString();
+
+  return function(...args: TParams) {
+    const hash = args
+      .map((arg) => {
+        switch (typeof arg) {
+          case "string":
+            return arg;
+          case "object":
+            return JSON.stringify(arg);
+          case "function":
+            return arg.toString();
+          case "number":
+            return arg.toString();
+          default:
+            return String(arg);
+        }
+      })
+      .join(sep);
+
+    if (cache.has(hash)) return cache.get(hash)!;
+
+    const result = func(...args);
+    cache.set(hash, result);
+    return result;
+  };
+}
+
+const levDistanceImpl = (a: string, b: string) => {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  if (a[0] === b[0]) {
+    return levDistance(a.slice(1), b.slice(1));
+  }
+
+  return (
+    1 +
+    Math.min(
+      levDistance(a.slice(1), b),
+      levDistance(a, b.slice(1)),
+      levDistance(a.slice(1), b.slice(1))
+    )
+  );
+};
+
+export const levDistance = cached(levDistanceImpl);
