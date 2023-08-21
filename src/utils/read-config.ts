3130551f903592c9680d5f3b8d893cb617b2f367
ej-shafran <ejshafran@hilma.tech>
2023-08-21 13:41:40 +0300

    Update reading config logic

--- src/utils/read-config.ts
+++ src/utils/read-config.ts
@@ -18,15 +18,22 @@ const readFile = (fileName: string) => readFileSync(fileName, "utf-8");
 
 export function readConfig(fileName: string): ts.CompilerOptions {
   const configFile = ts.findConfigFile(dirname(fileName), fileExists);
-  if (configFile) {
-    const { config, error } = ts.readConfigFile(configFile, readFile);
-
-    if (!error) {
-      return config.compilerOptions;
-    } else {
-      return DEFAULT_COMPILER_OPTIONS;
-    }
-  } else {
+  if (!configFile) {
     return DEFAULT_COMPILER_OPTIONS;
   }
+
+  const { config, error } = ts.readConfigFile(configFile, readFile);
+  if (error) {
+    return DEFAULT_COMPILER_OPTIONS;
+  }
+
+  const { options, errors } = ts.convertCompilerOptionsFromJson(
+    config.compilerOptions,
+    fileName
+  );
+  if (errors) {
+    return DEFAULT_COMPILER_OPTIONS;
+  }
+
+  return options;
 }
