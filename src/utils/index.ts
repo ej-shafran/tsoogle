b92a47a3989f90bf5224b7601e2f5ff6e59da780
ej-shafran <ejshafran@hilma.tech>
2023-08-21 13:16:11 +0300

    Move everything into read-config

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
-export * from "./file-exists";
 export * from "./lev-distance";
 export * from "./read-config";
--- src/utils/read-config.ts
+++ src/utils/read-config.ts
@@ -1,13 +1,25 @@
 import { readFileSync } from "fs";
+import { dirname } from "path";
 import ts from "typescript";
+import { accessSync } from "fs";
+
+function fileExists(fileName: string) {
+  try {
+    accessSync(fileName);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
 
 const DEFAULT_COMPILER_OPTIONS: ts.CompilerOptions = {};
 
 const readFile = (fileName: string) => readFileSync(fileName, "utf-8");
 
-export function readConfig(fileName: string | undefined): ts.CompilerOptions {
-  if (fileName) {
-    const { config, error } = ts.readConfigFile(fileName, readFile);
+export function readConfig(fileName: string): ts.CompilerOptions {
+  const configFile = ts.findConfigFile(dirname(fileName), fileExists);
+  if (configFile) {
+    const { config, error } = ts.readConfigFile(configFile, readFile);
 
     if (!error) {
       return config.compilerOptions;
